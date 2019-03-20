962e8293c33296f47b7bfacba9c3a7d39b1efb75
Jonny Rodriguez <jojoroto@gmail.com>
2019-03-20 01:47:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var takeANumber = (line,name) => {
   line.push(name);
-  return `Welcome, $[name]. You are number $[line.length] in line.`;
+  return `Welcome, ${name}. You are number ${line.length} in line.`;
 };
 
 var nowServing = line => {
