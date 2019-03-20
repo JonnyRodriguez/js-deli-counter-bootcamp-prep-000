9909031f29d2259dd0e459d9f98c2b9e9b6a9140
Jonny Rodriguez <jojoroto@gmail.com>
2019-03-20 01:44:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ var takeANumber = (line,name) => {
   return `Welcome, $[name]. You are number $[line.length] in line.`;
 };
 
+var nowServing = line => {
+  if( line.length>0 ) {
+    let person=line.shift();
+    
+  }
+}
